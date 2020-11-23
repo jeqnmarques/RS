e29f67b6cab5081a39c024a02450e0724ad52df5
jeqnmarques <74734221+jeqnmarques@users.noreply.github.com>
2020-11-23 18:52:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,554 @@
+const stories = [
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/1_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/2_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/7_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/8_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/12_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/13_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/14_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/15_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/17_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/18_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/19_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/19_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/21_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/23_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/24_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/27_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/28_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/IMG_6122.MP4',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/31_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/33_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/36_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/52_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/37_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/38_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/40_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/41_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/43_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/45_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/47_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/48_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/49_CN_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/CN/50_CN_JEANMARQUES.JPG',
+    time: 4000
+  }
+]
+
+const stories_2 = [
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/1_BB_JEANMARQUES.JPG',
+    time: 6000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/2_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/3_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/5_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/6_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/7_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/8_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/9_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/10_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/11_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/12_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/13_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/14_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/15_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/16_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/17_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/18_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/20_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/21_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/22_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/23_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/24_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/25_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/42_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/26_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/27_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/28_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/29_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/30_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/31_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/33_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+  {
+    title: '',
+    description: '',
+    image: 'IMG/BB/34_BB_JEANMARQUES.JPG',
+    time: 4000
+  },
+]
+
+const container = document.querySelector('.container')
+const container_2 = document.querySelector('#container_2')
+const nextButton = document.querySelector('#next')
+const backButton = document.querySelector('#back')
+const nextButton_2 = document.querySelector('#next_2')
+const backButton_2 = document.querySelector('#back_2')
+
+function Storyfier(storiesArray, rootEl) {
+  this.stories = storiesArray
+  this.root = rootEl
+  this.times = rootEl.querySelector('#times')
+  this.currentTime = 0
+  this.currentIndex = 0
+
+  // create breakpoints for when the slides should change
+  this.intervals = this.stories.map((story, index) => {
+    // TODO change so that it just uses the previous value + current time
+    let sum = 0
+    for (let i = 0; i < index; i++){
+      sum += this.stories[i].time
+    }
+    return sum
+  })
+
+  // necessary to make sure the last slide plays to completion
+  this.maxTime = this.intervals[this.intervals.length - 1] + this.stories[this.stories.length - 1].time
+
+  // render progress bars
+  this.progressBars = this.stories.map(() => {
+    const el = document.createElement('div')
+    el.classList.add('time-item')
+    el.innerHTML = '<div style="width: 0%"></div>'
+    return el
+  })
+
+  this.progressBars.forEach((el) => {
+    this.times.appendChild(el)
+  })
+
+  // methods
+  this.render = () => {
+    const story = this.stories[this.currentIndex]
+    this.root.style.background = `url('${story.image}')`
+    this.root.querySelector('#title').innerHTML = story.title
+    this.root.querySelector('#description').innerHTML = story.description
+  }
+
+  this.updateProgress = () => {
+    this.progressBars.map((bar, index) => {
+      // Fill already passed bars
+      if (this.currentIndex > index) {
+        bar.querySelector('div').style.width = '100%'
+        return
+      }
+
+      if (this.currentIndex < index) {
+        bar.querySelector('div').style.width = '0%'
+        return
+      }
+
+      // update progress of current bar
+      if (this.currentIndex == index) {
+        const timeStart = this.intervals[this.currentIndex]
+
+        let timeEnd;
+        if (this.currentIndex == this.stories.length - 1){
+          timeEnd = this.maxTime
+        } else {
+          timeEnd = this.intervals[this.currentIndex + 1]
+        }
+
+        const animatable = bar.querySelector('div')
+        animatable.style.width = `${((this.currentTime - timeStart)/(timeEnd - timeStart))*100}%`
+
+
+      }
+    })
+  }
+}
+
+Storyfier.prototype.start = function(){
+  // Render initial state
+  this.render()
+
+  // START INTERVAL
+  const test = setInterval(() => {
+    this.currentTime += 10
+    this.updateProgress()
+
+    if (this.currentIndex >= this.stories.length - 1 && (this.currentTime > this.maxTime)){
+      clearInterval(test)
+      return
+    }
+
+    const lastIndex = this.currentIndex
+    if (this.currentTime >= this.intervals[this.currentIndex + 1]){
+      this.currentIndex += 1
+    }
+
+    if (this.currentIndex != lastIndex) {
+      this.render()
+    }
+  }, 10)
+}
+
+Storyfier.prototype.next = function(){
+  const next = this.currentIndex + 1
+  if (next > this.stories.length - 1){
+    return
+  }
+
+  this.currentIndex = next
+  this.currentTime = this.intervals[this.currentIndex]
+  this.render()
+}
+
+Storyfier.prototype.back = function(){
+  if ((this.currentTime > (this.intervals[this.currentIndex] + 350)) || this.currentIndex === 0){
+    this.currentTime = this.intervals[this.currentIndex]
+    return
+  }
+
+  this.currentIndex -= 1
+  this.currentTime = this.intervals[this.currentIndex]
+  this.render()
+}
+
+const setup = async () => {
+  const loadImages = stories.map(({ image }) => {
+    return new Promise((resolve, reject) => {
+      let img = new Image()
+      img.onload = () => {
+        resolve(image)
+      }
+      img.src = image
+    })
+  })
+
+  await Promise.all(loadImages)
+
+  const s = new Storyfier(stories, container);
+  s.start()
+
+  nextButton.addEventListener('click', () => {
+    s.next()
+  })
+
+  backButton.addEventListener('click', () => {
+    s.back()
+  })
+
+  const s_2 = new Storyfier(stories_2, container_2);
+  s_2.start()
+
+  nextButton_2.addEventListener('click', () => {
+    s_2.next()
+  })
+
+  backButton_2.addEventListener('click', () => {
+    s_2.back()
+  })
+}
+
+setup()
